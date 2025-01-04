c4c42791405fcde5f8cdc644c73d209b52793024
“jorjew <66022792up.ac.th>
2025-01-04 23:58:05 +0700

    name

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,28 @@
-export default function Home () {
-return (
-   <>
-    <div class="bg-purple-300 h-32"></div>
+export default function Home() {
+  return (
+    <>
+       <div className="bg-purple-700 h-32"></div>
+       
+       <div className="max-w-xl mx-auto -mt-10"> 
+        <div className="bg-white shadow-lg rounded-lg p-4">
+          <div className="flex items-center">
+           
+          <div className="w-24 h-24">
+            <img
+                src="https://scontent.xx.fbcdn.net/v/t1.15752-9/466657307_1574267493459544_4022526027368134532_n.jpg?_nc_cat=106&ccb=1-7&_nc_sid=0024fc&_nc_ohc=cjOegaUMd6EQ7kNvgE8hWhV&_nc_ad=z-m&_nc_cid=0&_nc_zt=23&_nc_ht=scontent.xx&oh=03_Q7cD1gHI7-WMdKc8Nsq90AwIVFiCp4n8pRU2gcLu6_zxg_gqBQ&oe=67A0DF33"
+                alt="Profile"
+                className="w-full h-full object-cover rounded-full"
+                />
+          </div>
+            <div className="ml-6">
+              <h1 className="text-2xl font-semibold">Phongsakron Chuaichoenpradit </h1>
+              <p className="text-black">Jew</p>
+              <p className="text-gray-500">Front-End Developer</p>
+            </div>
+          </div>
+          </div>
+          </div>
 
-    
-   
 
 
 
@@ -12,13 +30,14 @@ return (
 
 
 
-   </>
 
 
 
 
 
-);
 
 
+
+    </>
+  );
 }
